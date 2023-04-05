78a4836d1a52063a6a623538c3f96939ea5da093
marciomaia2020 <marciofernando.maia@yahoo.com.br>
2023-04-05 20:11:56 -0300

    Update files

---
+++ script.js
@@ -0,0 +1,53 @@
+/*
+O JavaScript pode "exibir" dados de diferentes maneiras:
+
+Gravando em um elemento HTML, usando .innerHTML
+Gravando na saída HTML usando o .document.write()
+Gravando em uma caixa de alerta, usando o .window.alert()
+Gravando no console do navegador, usando o .console.log()
+*/
+
+
+
+
+
+/*
+Para acessar um elemento HTML, o JavaScript pode usar o método. document.getElementById(id)
+O atributo define o elemento HTML. A propriedade define o conteúdo HTML:idinnerHTML */
+/*ADICIONE UM DEMO NO HTML
+document.getElementById("demo").innerHTML = 5 + 6; 
+*/
+
+
+
+
+
+
+/*
+Para fins de teste, é conveniente usar:document.write() */
+/*O uso de document.write() depois que um documento HTML for carregado, excluirá todo o HTML:*/
+/*document.write(5 + 6);*/
+/*O método document.write() só deve ser usado para teste.*/
+
+/*O método document.write() só deve ser usado para teste.*/
+/*window.alert(5 + 6);/*EXPLICAR COMO FUNCIONA O JAVASCRIPT*/
+
+
+
+
+
+/*
+Você pode pular a palavra-chave.window
+Em JavaScript, o objeto window é o objeto de escopo global. 
+Isso significa que variáveis, propriedades e métodos, por padrão, pertencem ao objeto window. 
+Isso também significa que especificar a palavra-chave é opcional:window*/
+/*alert(5 + 6);*/
+
+
+
+
+/*
+F12 no teclado ativará a depuração.
+Em seguida, selecione "Console" no menu do depurador.
+Em seguida, clique em Executar novamente.*/
+/*console.log(5 + 6);*/
