e4df7380f3547b4abfe7e7460e7ffa31b776374a
marciomaia2020 <marciofernando.maia@yahoo.com.br>
2023-04-14 07:23:16 -0300

    Update Files

--- script.js
+++ script.js
@@ -1,24 +1,2 @@
 window.alert("Márcio Fernanando Maia");
 
-/* CODIGO PARA O EXERCÍCIO - 
-<h2>Meu segundo código em JavaScript</h2>
-
-<button type="button"
-onclick="document.getElementById('demo').innerHTML = Date()">
-Clique aqui par aparecer a hora atual</button>
-
-<p id="demo"></p>
-*/
-
-
-/*
-Aula dia 20-04
-G:\Meu Drive\Escolas\CelioRodriguesAlves\SegundoAnoTI\Preparação para aula dia 20-04
-Seguir ordem 
-a)
-b)
-c)
-Reforçar Fork, E Pull Requeste
-Explicar usando TV
-Exercícios
-
